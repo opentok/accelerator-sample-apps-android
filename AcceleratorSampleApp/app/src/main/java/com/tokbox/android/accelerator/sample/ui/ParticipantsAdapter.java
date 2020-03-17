2166fd29e32210bcce6fd7b8cc2741c3719ffe70
Marina Serrano Montes <mserranomontes@MacBook-Pro-de-Marina-2.local>
2020-03-17 09:54:52 +0100

    Update deps versions

--- AcceleratorSampleApp/app/src/main/java/com/tokbox/android/accelerator/sample/ui/ParticipantsAdapter.java
+++ AcceleratorSampleApp/app/src/main/java/com/tokbox/android/accelerator/sample/ui/ParticipantsAdapter.java
@@ -1,7 +1,7 @@
 package com.tokbox.android.accelerator.sample.ui;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

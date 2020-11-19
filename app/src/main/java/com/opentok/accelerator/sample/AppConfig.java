a7bcfd8d91823da93f2a8423f399d19681b9ccda
igor.wojda <igor.wojda@gmail.com>
2020-11-19 11:38:36 +0100

    Update config

--- app/src/main/java/com/opentok/accelerator/sample/AppConfig.java
+++ app/src/main/java/com/opentok/accelerator/sample/AppConfig.java
@@ -5,15 +5,14 @@ import com.opentok.accelerator.core.utils.OTConfig;
 class AppConfig {
     // Fill the following arguments for your OpenTok Project. All the required information can be found in the OpenTok
     // dashboard
-    // https://dashboard.tokbox.com/projects
+    // https://tokbox.com/account
 
     static final OTConfig otConfig = new OTConfig(
             "", // Replace with session Id from the dashboard
             "", // Replace with a generated token from the dashboard or using an OpenTok server SDK
-            "OpenTokConfig.API_KEY", // Replace with your OpenTok API key from the dashboard
+            "", // Replace with your OpenTok API key from the dashboard
             "accelerator-sample-app",
             false,
             true
     );
 }
-

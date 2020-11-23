8c10841f3d2f78add6b6390d7cf879456d4a504c
igor.wojda <igor.wojda@gmail.com>
2020-11-23 20:12:53 +0100

    Added named agruments

--- app/src/main/java/com/opentok/accelerator/sample/AppConfig.kt
+++ app/src/main/java/com/opentok/accelerator/sample/AppConfig.kt
@@ -2,17 +2,16 @@ package com.opentok.accelerator.sample
 
 import com.opentok.accelerator.core.utils.OTConfig
 
-internal object AppConfig {
+object AppConfig {
     // Fill the following arguments for your OpenTok Project. All the required information can be found in the OpenTok
     // dashboard
     // https://tokbox.com/account
-    @JvmField
     val otConfig = OTConfig(
-        "",  // Replace with API key from the dashboard
-        "",  // Replace with a session id from the dashboard (or an OpenTok server SDK)
-        "",  // Replace with token from the dashboard (or an OpenTok server SDK)
-        "accelerator-sample-app",
-        false,
-        true
+        apiKey = "",  // Replace with API key from the dashboard
+        sessionId = "",  // Replace with a session id
+        token = "" , // from the dashboard (or an OpenTok server SDK)
+        sessionName = "accelerator-sample-app",
+        subscribeToSelf = false,
+        subscribeAutomatically = true
     )
 }

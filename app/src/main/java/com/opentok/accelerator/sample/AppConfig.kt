eed429c4534f2836239b4e5c571c334ebfbf4d78
igor.wojda <igor.wojda@gmail.com>
2020-11-24 11:56:23 +0100

    Update URL

--- app/src/main/java/com/opentok/accelerator/sample/AppConfig.kt
+++ app/src/main/java/com/opentok/accelerator/sample/AppConfig.kt
@@ -3,13 +3,12 @@ package com.opentok.accelerator.sample
 import com.opentok.accelerator.core.utils.OTConfig
 
 object AppConfig {
-    // Fill the following arguments for your OpenTok Project. All the required information can be found in the OpenTok
-    // dashboard
-    // https://tokbox.com/account
+    // Fill the following arguments for your OpenTok Project. All the required information can be found in the dashboard
+    // https://dashboard.tokbox.com/
     val otConfig = OTConfig(
         apiKey = "",  // Replace with API key from the dashboard
         sessionId = "",  // Replace with a session id
-        token = "" , // from the dashboard (or an OpenTok server SDK)
+        token = "" , // Replace with a token from the dashboard (or the OpenTok server SDK)
         sessionName = "accelerator-sample-app",
         subscribeToSelf = false,
         subscribeAutomatically = true
